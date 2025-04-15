a4a4167e01871801c7a1a5c28cf256e2de1b7627
Rishabh Rawat <rishabh@remoteward.com>
2025-04-15 23:28:27 +0530

    Topic: Added port number to example endpoint

--- service-discovery/example-service/src/main/java/com/example/exampleservice/controller/ExampleController.java
+++ service-discovery/example-service/src/main/java/com/example/exampleservice/controller/ExampleController.java
@@ -1,13 +1,20 @@
 package com.example.exampleservice.controller;
 
+import org.springframework.beans.factory.annotation.Value; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleController {
 
+
+    // Inject the server port from application properties
+    @Value("${server.port}")
+    private String serverPort;
+
+
     @GetMapping("/example")
     public String exampleEndpoint() {
-        return "Hello from Example Service!";
+       return String.format("Hello from Example Service on port %s!", serverPort);
     }
 }
